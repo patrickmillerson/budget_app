2f48f3cdabcb536e549a4ef24aade6d2a2d8b3e1
Patrick Millerson <patmillerson@gmail.com>
2023-12-04 18:09:40 -0400

    Commented Out Code

--- budget_app/admin.py
+++ budget_app/admin.py
@@ -4,6 +4,6 @@ from budget_app.models import Expense, ExpenseCategory, Income
 
 # Register your models here.
 
-admin.site.register(Income)
-admin.site.register(ExpenseCategory)
-admin.site.register(Expense)
+# admin.site.register(Income)
+# admin.site.register(ExpenseCategory)
+# admin.site.register(Expense)
